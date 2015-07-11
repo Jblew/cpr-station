b494196f63fc6f8ad03318d764e84783b35c31ca
Jblew <jedrzejblew@gmail.com>
2015-07-11 13:28:04 +0200

    Git init

---
+++ src/main/java/pl/jblew/cpr/App.java
@@ -0,0 +1,13 @@
+package pl.jblew.cpr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
